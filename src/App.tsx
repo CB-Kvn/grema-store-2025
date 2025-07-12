3cb9b7f4d93ecefbdd273b2087eba300e3044b40
CB-Kvn <150515929+CB-Kvn@users.noreply.github.com>
2025-07-12 12:30:38 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
